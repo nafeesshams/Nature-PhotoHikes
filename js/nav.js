8f39c267881ba18045d8f21129fb5e1dbfab352f
nafeesshams <nafeesshams@gmail.com>
2020-09-25 23:48:08 -0700

    1st commit

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+document.write('\
+\
+
+<nav id="nav">\
+			<ul>\
+			<li><a href="index.html" class="logo" id="logo"><img src="images/logo.png"></a></li>\
+			<li><a href="#">Hiking Tours</a>\
+			<ul>\
+				<li><a href="hikes.html">Day Hikes</a></li>\		
+				<li><a href="hikes_multiple.html">Multiple Day Hikes</a></li>\
+			</ul>\
+			</li>\
+			<li><a href="international.html">International Tours</a></li>\
+			<li><a href="blog.html">Blog</a></li>\
+			<li><a href="contact.html">Contact</a></li>\
+			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>\
+			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>\
+			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>\
+			<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>\
+			<li><a href="#" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>\
+			</ul>\
+			</nav>\
+
+\
+');
